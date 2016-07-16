ab9fc99f8b977154a15d45ddb35e907cd245a205
Pawel Kowalak <pawel.kowalak@gmail.com>
2016-07-16 19:00:50 +0200

    Stop failing when no changes to be deployed (attempt #2)

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -4,4 +4,4 @@ cd "$(dirname "$0")/public" || exit 0
 git config user.email "pawel.kowalak@gmail.com"
 git config user.name "Pawel Kowalak"
 git add -A
-git commit -m "Auto-deploy for build $CIRCLE_BUILD_NUM" && git push origin master
+git commit -m "Auto-deploy for build $CIRCLE_BUILD_NUM" && git push origin master || exit 0

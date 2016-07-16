b9030e4baf557aec76955f3351637fa855ba1fbe
Pawel Kowalak <pawel.kowalak@gmail.com>
2016-07-16 18:54:37 +0200

    Stop failing builds when nothing new to commit

--- ci-deploy.sh
+++ ci-deploy.sh
@@ -1,8 +1,7 @@
-#!/bin/bash -ex
+#!/bin/bash -x
 
-cd "$(dirname "$0")/public"
+cd "$(dirname "$0")/public" || exit 0
 git config user.email "pawel.kowalak@gmail.com"
 git config user.name "Pawel Kowalak"
 git add -A
-git commit -m "Auto-deploy for build $CIRCLE_BUILD_NUM"
-git push origin master
+git commit -m "Auto-deploy for build $CIRCLE_BUILD_NUM" && git push origin master
